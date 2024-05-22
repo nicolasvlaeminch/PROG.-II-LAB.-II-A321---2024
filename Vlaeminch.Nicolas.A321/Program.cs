35b23170d367ad33c3e75a75f41a5ce8352387b3
Nicolas Vlaeminch <nicolasvlaeminch777@gmail.com>
2024-05-22 18:15:19 -0300

    upload

--- Vlaeminch.Nicolas.A321/Program.cs
+++ Vlaeminch.Nicolas.A321/Program.cs
@@ -10,6 +10,8 @@ namespace Vlaeminch.Nicolas.A321
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Test Push");
+            Console.ReadLine();
         }
     }
 }
